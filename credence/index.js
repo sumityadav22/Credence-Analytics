0ab5f98903a8d1a3e80dd5bf1ac5815e45bac9b3
sumityadav22 <sumityadavs5574@gmail.com>
2020-07-26 02:28:36 +0530

    ADD - function for fetching data

--- credence/index.js
+++ credence/index.js
@@ -2,3 +2,15 @@ const http = require('http');
 const fs = require('fs')
 const axios = require('axios');
 const handlebars = require('handlebars');
+
+var data_file = 'wheather-data.json';
+var data_url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+
+function fetchData() {
+  axios.get(data_url).then(resp => {
+      fs.writeFile(data_file, JSON.stringify(resp.data), function (err) {
+        if (err) throw err;
+        console.log('Data saved in: ', data_file);
+      });
+  });
+}
